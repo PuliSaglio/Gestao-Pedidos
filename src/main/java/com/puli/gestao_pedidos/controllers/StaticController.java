79e6fd7e6eb641e10e17431b499b28f0767ab02c
PuliSaglio <pulisaglio@gmail.com>
2025-02-20 11:05:26 -0300

    tentando fazer funcionar o js

--- src/main/java/com/puli/gestao_pedidos/controllers/StaticController.java
+++ src/main/java/com/puli/gestao_pedidos/controllers/StaticController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/static")
 public class StaticController {
+    //tive que fazer essa gambiarra porque o static não estava funcionando
 
     @GetMapping("/css/estilo.css")
     public ResponseEntity<Resource> getCss() {
@@ -19,6 +20,42 @@ public class StaticController {
     @GetMapping("/css/index.css")
     public ResponseEntity<Resource> getIndexCss() {
         Resource resource = new ClassPathResource("static/css/index.css");
+        return ResponseEntity.ok().body(resource);
+    }
+
+    @GetMapping("/js/clientes.js")
+    public ResponseEntity<Resource> getClientesJs() {
+        Resource resource = new ClassPathResource("static/js/clientes.js");
+        return ResponseEntity.ok().body(resource);
+    }
+
+    @GetMapping("/js/pedidos.js")
+    public ResponseEntity<Resource> getPedidosJs() {
+        Resource resource = new ClassPathResource("static/js/pedidos.js");
+        return ResponseEntity.ok().body(resource);
+    }
+
+    @GetMapping("/js/produtos.js")
+    public ResponseEntity<Resource> getProdutosJs() {
+        Resource resource = new ClassPathResource("static/js/produtos.js");
+        return ResponseEntity.ok().body(resource);
+    }
+
+    @GetMapping("/js/item-pedidos.js")
+    public ResponseEntity<Resource> getItemPedidosJs() {
+        Resource resource = new ClassPathResource("static/js/item-pedidos.js");
+        return ResponseEntity.ok().body(resource);
+    }
+
+    @GetMapping("/js/formata-string.js")
+    public ResponseEntity<Resource> getFormataStringJs() {
+        Resource resource = new ClassPathResource("static/js/formata-string.js");
+        return ResponseEntity.ok().body(resource);
+    }
+
+    @GetMapping("/js/relatorios.js")
+    public ResponseEntity<Resource> getRelatoriosJs() {
+        Resource resource = new ClassPathResource("static/js/relatorios.js");
         return ResponseEntity.ok().body(resource);
     }
 }
