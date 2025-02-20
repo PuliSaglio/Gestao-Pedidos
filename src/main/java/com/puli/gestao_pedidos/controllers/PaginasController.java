bc0a404dc18fc9fbf04ba07cf0d22ae23c30ee80
PuliSaglio <pulisaglio@gmail.com>
2025-02-20 09:43:41 -0300

    update PaginasController

--- src/main/java/com/puli/gestao_pedidos/controllers/PaginasController.java
+++ src/main/java/com/puli/gestao_pedidos/controllers/PaginasController.java
@@ -3,43 +3,44 @@ package com.puli.gestao_pedidos.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/")
 public class PaginasController {
 
     @GetMapping
-    public String home() {
-        return "index";
+    public ModelAndView home() {
+        return new ModelAndView("index");
     }
 
     @GetMapping("/clientes")
-    public String clientes() {
-        return "clientes";
+    public ModelAndView clientes() {
+        return new ModelAndView("clientes");
     }
 
     @GetMapping("/produtos")
-    public String produtos() {
-        return "produtos";
+    public ModelAndView produtos() {
+        return new ModelAndView("produtos");
     }
 
     @GetMapping("/pedidos")
-    public String pedidos() {
-        return "pedidos";
+    public ModelAndView pedidos() {
+        return new ModelAndView("pedidos");
     }
 
     @GetMapping("/item-pedidos")
-    public String itemPedidos() {
-        return "item-pedidos";
+    public ModelAndView itemPedidos() {
+        return new ModelAndView("item-pedidos");
     }
 
     @GetMapping("/relatorios")
-    public String relatorios() {
-        return "relatorios";
+    public ModelAndView relatorios() {
+        return new ModelAndView("relatorios");
     }
 
     @GetMapping("/formata-string")
-    public String formataString() {
-        return "formata-string";
+    public ModelAndView formataString() {
+        return new ModelAndView("formata-string");
     }
 }
